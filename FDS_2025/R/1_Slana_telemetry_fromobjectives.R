157494540b94b3cd440339ab184ce542fcc94d48
Matt Tyers <matt.tyers@alaska.gov>
2025-12-10 15:58:25 -0900

    defined lake, continued summary stats

--- FDS_2025/R/1_Slana_telemetry_fromobjectives.R
+++ FDS_2025/R/1_Slana_telemetry_fromobjectives.R
@@ -1,35 +1,6 @@
 library(riverdist)
 library(tidyverse)
 
-# slanacopper0 <- line2network(path="FDS_2025/raw_data",
-#                             layer="SlanaCopper2",
-#                             tolerance=20)
-# slanacopper1 <- cleanup(slanacopper0)
-# plot(slanacopper1)
-# plot(slanacopper1, empty=TRUE)
-# save(slanacopper1, file="FDS_2025/flat_data/slanacopper1.Rdata")
-#
-#
-# ptdata <- read.csv("FDS_2025/flat_data/GIS.csv") %>%
-#   select(Unique, Survey, Length, Sex, Date, Freq, Code, Fate, Latitude, Longitude) %>%
-#   filter(!is.na(Unique))
-#
-# # transform point data to river coords
-# # make sure river is in alaska albers
-# ptdata_akalbers <- select(ptdata, c("Longitude","Latitude")) %>%
-#   sf::sf_project(pts=.,
-#                  to="+proj=aea +lat_1=55 +lat_2=65
-#     +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs
-#     +ellps=GRS80")
-# ptdata_segvert <- xy2segvert(x=ptdata_akalbers[,1],
-#                              y=ptdata_akalbers[,1],
-#                              rivers=slanacopper1)
-#
-#
-# zoomtoseg(c(206,11), slanacopper1)#, empty=TRUE)
-# zoomtoseg(c(210,100), slanacopper1)#, empty=TRUE)
-# points(ptdata_akalbers)
-
 
 
 
@@ -69,28 +40,115 @@ ptdata$vert <- ptdata_segvert$vert
 ptdata$x <- ptdata_akalbers[,1]
 ptdata$y <- ptdata_akalbers[,2]
 
+
+
 # 1.	net distance traveled between tracking events,
-distance_seq <- with(ptdata, riverdistanceseq(unique=Code,
-                              seg=seg, vert=vert,
-                              survey=Survey,
-                              rivers=slanacopper1))/1000
-## SHOULD PROBABLY TRY THIS WITH NON MORTS
+# distance_seq <- with(ptdata,
+#                      riverdistanceseq(unique=Code,
+#                                       seg=seg, vert=vert,
+#                                       survey=Survey,
+#                                       rivers=slanacopper1))/1000
+
+# subset of fish that are Alive
+distance_seq <- with(subset(ptdata, Fate=="A"),
+                     riverdistanceseq(unique=Code,
+                                      seg=seg, vert=vert,
+                                      survey=Survey,
+                                      rivers=slanacopper1))/1000
+
 
 # 2.	direction traveled between tracking events,
-direction_seq <- with(ptdata, riverdirectionseq(unique=Code,
-                                              seg=seg, vert=vert,
-                                              survey=Survey,
-                                              rivers=slanacopper1))
+# direction_seq <- with(ptdata,
+#                       riverdirectionseq(unique=Code,
+#                                         seg=seg, vert=vert,
+#                                         survey=Survey,
+#                                         rivers=slanacopper1))
+
+# subset of fish that are Alive
+direction_seq <- with(subset(ptdata, Fate=="A"),
+                      riverdirectionseq(unique=Code,
+                                        seg=seg, vert=vert,
+                                        survey=Survey,
+                                        rivers=slanacopper1))
 raw_dirtable <- apply(direction_seq, 2,
       \(x) table(factor(x, levels=c("down","0","up"))))
 mosaicplot(t(raw_dirtable))
-## SHOULD PROBABLY TRY THIS WITH NON MORTS
+
 
 # 3.	net distance traveled between presumed spawning, summering, and
 #     overwintering locations; and,
 # 4.	annual home range, defined as the distance between the furthest upstream
 #     and furthest downstream locations of individual fish over the course of a year.
+
+# FIGURE OUT WHICH IS APPROPRIATE
+hr <- with(ptdata,
+           homerange(unique = Code,
+                     survey = Date,
+                     seg = seg,
+                     vert = vert,
+                     rivers = slanacopper1))
+hr <- with(subset(ptdata, Fate=="A"),
+           homerange(unique = Code,
+                     survey = Date,
+                     seg = seg,
+                     vert = vert,
+                     rivers = slanacopper1))
+hr$ranges/1000 ## this is what will be applicable
+### also calculate total observed distance moved (and maybe add this to riverdist)
+
+by_indiv <- data.frame(n_surveys = rep(NA, length(unique(ptdata$Code))),
+                       homerange = NA,
+                       totaldist = NA)
+ptdataA <- subset(ptdata, Fate=="A")
+for(codei in sort(unique(ptdata$Code))) {
+  by_indiv$n_surveys[i] <- with(subset(ptdataA, Code==codei), length(unique(Survey)))
+
+  if(by_indiv$n_surveys[i] > 1) {
+    by_indiv$homerange[i] <- with(subset(ptdataA, Code==codei),
+                                  homerange(seg=seg,
+                                            vert=vert,
+                                            rivers=slanacopper1))$ranges$range/1000
+
+    # this is where i would calculate the total distance for the subset codei
+    xx <- subset(ptdataA, Code==codei)
+  }
+}
+
+
+
+
 # % in/outside of the lake by survey (probably nonmorts)
+
+
+# is it maybe easier to look at points via leaflet??
+library(leaflet)
+leaflet(ptdata) %>%
+  addTiles() %>%
+  # addProviderTiles("Esri.WorldImagery") %>%#,
+                   # options=providerTileOptions(opacity=.5)) %>%
+  # addMarkers(lng= ~longitude,
+  #            lat= ~latitude,
+  #            label= ~unique_id...2)
+  addCircles(lng= ~Longitude,
+             lat= ~Latitude,
+             label= ~Survey,
+             color= ~ifelse(Date=="2024-10-16", "red", "blue"),
+             opacity = 1)
+             # color= ~rainbow(17)[as.numeric(as.factor(Survey))])
+
+
+
+# figuring out which segments represent the lake
+plot(slanacopper1)
+zoomtoseg(c(110,43),slanacopper1)
+points(ptdata_akalbers)
+points(ptdata_akalbers[ptdata$Date=="2024-10-16",], pch=15, col=4)
+lake <- ptdata$y > max(slanacopper1$lines[[65]][,2])
+
+zoomtoseg(c(110,181),slanacopper1)
+points(ptdata_akalbers, pch=16, col=ifelse(lake, 3, 2))
+ptdata$lake <- lake
+
 # % in/outside of spawning area
 
 # figuring out which segments represent spawning area
@@ -128,7 +186,19 @@ ptdata$spawn <- spawn
 #                                   method="snaproute")
 
 
+tracker <- read.csv("FDS_2025/flat_data/tracker.csv")[1:100,]
+with(tracker, table(X6.11.2025, X7.22.2025))
+
 # questions:
 # - can I actually interpret the A/M in the GIS table as alive/mort?
 # - are there specific surveys I should consider spawning/summering/overwintering?
 # - how is spawning area defined?
+
+
+# ideas:
+# - hidden markov survival model!! maybe with explanatory variables?
+# - sankey/discrete sankey in/out of lake
+# - table by individual:
+#   * home range
+#   * prop of surveys in/out of lake
+#   * prop of surveys in/out of spawning
